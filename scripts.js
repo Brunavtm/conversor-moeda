8a4fd5e65f43b3eab9e34b21ea90e5198356cedc
Bruna <brunavtm@gmail.com>
2024-10-01 22:02:40 -0300

    Iniciando variaveis
    Iniciando variaveis

---
+++ scripts.js
@@ -0,0 +1,47 @@
+// Cotação das moedas
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+
+// Obtendo elementos do form
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+// Manipulando input para receber apenas números usando regex
+amount.addEventListener("input", () => {
+    const hasCharactersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharactersRegex, "")
+})
+
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    switch(currency.value){
+        case "USD":
+            convertCurrency(amount.value, USD, '$')
+            break
+        
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+    }
+
+}
+
+function convertCurrency(amount, price, symbol) {
+    try{
+        footer.classList.add("show-result")
+    }
+    catch(error){
+        console.log(error)
+        footer.classList.remove("")
+        alert("Não foi possível converter. Tente novamente mais tarde.")
+    }
+}
+
